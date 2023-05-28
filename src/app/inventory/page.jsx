e95832f6114e3090b694297cbd884ae65e632e61
Villy15 <adrianvill07@gmail.com>
2023-05-28 21:13:58 +0800

    Added sample data

--- src/app/inventory/page.jsx
+++ src/app/inventory/page.jsx
@@ -3,31 +3,57 @@
 import { useEffect, useState } from "react"
 import getIngredients from "../api/inventory/getIngredients";
 
-
 export default function Inventory() {
+    // https://www.youtube.com/watch?v=EaxC_kOG03E
     const [ingredients, setIngredients] = useState([])
 
     useEffect(() => {
-          fetchIngredients();
+        fetchIngredients();
     }, []);
 
     async function fetchIngredients() {
         try {
-          const fetchedIngredients = await getIngredients();
-          setIngredients(fetchedIngredients);
+            const fetchedIngredients = await getIngredients();
+            setIngredients(fetchedIngredients);
         } catch (error) {
-          console.error(error);
+            console.error(error);
         }
-      }
+    }
+
+    function getTableHeaders() {
+        if (ingredients.length === 0) {
+            return null;
+        }
+
+        const ingredientKeys = Object.keys(ingredients[0]);
+        return ingredientKeys.map((key) => (
+            <th key={key}>{key}</th>
+        ));
+    }
+
+    function getTableData() {
+        return ingredients.map((ingredient) => (
+            <tr key={ingredient.id}>
+                {Object.values(ingredient).map((value, index) => (
+                    <td key={index}>{value}</td>
+                ))}
+            </tr>
+        ));
+    }
 
     return (
-        <div>
+        <div className="inventory">
             <h1>Inventory</h1>
-            <ul>    
-                {ingredients.map((ingredient) => (
-                    <li key={ingredient.id}>{ingredient.name}</li>
-                ))}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        {getTableHeaders()}
+                    </tr>
+                </thead>
+                <tbody>
+                    {getTableData()}
+                </tbody>
+            </table>
         </div>
     )
 }
