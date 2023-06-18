45d5ac0f1a71ca629a79db025c013ec3e3bab49d
Villy15 <adrianvill07@gmail.com>
2023-06-18 18:31:45 +0800

    Add pos template

---
+++ src/pages/api/dish/getDish.js
@@ -0,0 +1,21 @@
+import { withSessionRoute } from "@lib/withSession";
+import supabase from "@supabase";
+
+export default withSessionRoute(getDish);
+
+async function getDish(req, res) {
+    try {
+        let { data: ingredients, error } = await supabase
+            .from('dish')
+            .select('*');
+
+        if (error) {
+            throw error;
+        }
+
+        res.send(ingredients);
+    } catch (error) {
+        res.statusCode = 500;
+        res.send({ error: "Server error" });
+    }
+}
--- src/pages/pos/index.jsx
+++ src/pages/pos/index.jsx
@@ -3,7 +3,7 @@ import Sidebar from "@components/Sidebar";
 
 import { withSessionSsr } from "@lib/withSession";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const getServerSideProps = withSessionSsr(
   async function getServerSideProps({ req, res }) {
@@ -25,12 +25,35 @@ export const getServerSideProps = withSessionSsr(
 export default function POS({ user }) {
   const router = useRouter();
 
+  const [dishes, setDishes] = useState([]);
+
   useEffect(() => {
     if (user.role === "Guest") {
       router.push("/login");
+    } else {
+      getDish();
     }
   }, [user, router]);
 
+  async function getDish() {
+    try {
+      const response = await fetch('/api/dish/getDish', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Request failed with status ' + response.status);
+      }
+
+      const data = await response.json();
+      setDishes(data);       
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
     <>
@@ -38,6 +61,27 @@ export default function POS({ user }) {
         <Sidebar role={user.role}/>
         <div className="main-section">
           <Header page={"POS"} user={user} />
+          <div className="pos-system">
+            <div className="pos-left">
+              <div className="dishes">
+                {/* Map dishes */}
+                {dishes.map((dish) => (
+                  <button key={dish.id}>
+                    <div className="dish-item">
+                      <div className="dish-image"> 
+                          {dish.dishName.toUpperCase()} 
+                      </div>
+                      <div className="dish-name">{dish.dishName}</div>
+                      <div className="dish-price">${dish.price}</div>
+                    </div>
+                  </button>
+                ))}
+              </div>
+            </div>
+            <div className="pos-right">
+
+            </div>
+          </div>
         </div>
       </main>
     </>
