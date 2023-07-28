9e17fc49c7d45d2cbb33d79429fac9ca8dedcb77
seanfolkard <sean_folkard@dlsu.edu.ph>
2023-07-29 00:05:09 +0800

    Revert "Changed input image to a upload/drag box"
    This reverts commit e620358d54d211d253181df06510f0676bc135cf.

--- components/Dish/AddDishForm.jsx
+++ components/Dish/AddDishForm.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { FileUploader } from 'react-drag-drop-files';
 
 async function fetchAPI(url) {
     const response = await fetch(url, {
@@ -46,7 +45,6 @@ async function fetchAPI(url) {
     return data;
   }
   
-const fileTypes = ['JPEG', 'JPG', 'PNG'];
 
 const AddDishForm = () => {
     const router = useRouter();
@@ -82,14 +80,6 @@ const AddDishForm = () => {
         setIngredients(updatedIngredients);
       };
 
-    const [file, setFile] = useState(null);
-
-    const handleChange = (file) => {
-        // Assuming you want to handle only one file for the image, so we'll take the first one from the array.
-        // If you want to handle multiple images, modify accordingly.
-        setFile(file[0]);
-    };
-
     const handleSubmit = (e) => {
         e.preventDefault();
     
@@ -98,7 +88,6 @@ const AddDishForm = () => {
           alert('Please fill out all required fields');
           return;
         }
-        
     
         // Construct the new dish object
         const newDish = {
@@ -108,11 +97,6 @@ const AddDishForm = () => {
           description: formValues.description,
           ingredients,
         };
-
-          // Append the image file to the newDish object (if present)
-        if (file) {
-            newDish.image = file.preview;
-        }
     
         // Perform the necessary actions with the new dish data (e.g., save to API)
         postDish(newDish);
@@ -126,7 +110,6 @@ const AddDishForm = () => {
           image: '',
         });
         setIngredients([]);
-        setFile(null); // Reset the selected file after form submission
       };
 
     return (
@@ -184,15 +167,15 @@ const AddDishForm = () => {
                             />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="image">Insert Image: </label>
-                            <FileUploader
-                                multiple={true}
-                                handleChange={handleChange}
-                                name="image"
-                                types={fileTypes}
-                                uploadText="Drop or click to select an image"
-                            />
-                        <p>{file ? `File name: ${file.name}` : 'no image selected'}</p>
+                        <label htmlFor="image">Input Image URL CHANGE TO DRAG PICTURE: </label>
+                        <input
+                            type="text"
+                            name="image"
+                            id="image"
+                            className='description'
+                            value={formValues.image}
+                            onChange={handleInputChange}
+                        />
                     </div>  
                 </div>
                 {/* Add Table where you can input ingredients, quantity, and unit */}
